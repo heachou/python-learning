20725eb89e5fb0c1aa849dafe5df7bdf6ed6e8a8
axl <1544500164@qq.com>
2019-11-04 09:52:25 +0800

    tuple

--- src/tuple.py
+++ src/tuple.py
@@ -10,7 +10,17 @@ print(b)  # ('a', 'b', 'c')
 
 # zip
 a = [10, 20, 30]
-b = [30, 40, 50]
+b = [30, 40, 50, 60]
 c = zip(a, b)
 print(c)  # <zip object at 0x03607760>
 print(list(c))  # [(10, 30), (20, 40), (30, 50)]
+
+s = (x * 2 for x in range(5))
+print(tuple(s))  # (0, 2, 4, 6, 8)
+print(tuple(s))  # () 只能用一次
+
+s = (x * 2 for x in range(6))
+
+print(s.__next__())  # 0
+print(s.__next__())  # 2
+print(s.__next__())  # 4
