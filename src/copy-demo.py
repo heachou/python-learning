3cb9d9def1e7dc85a585580852c3cfbaf8a8d67a
heachou <1244239990@qq.com>
2019-11-07 18:05:43 +0800

    深拷贝

--- src/copy-demo.py
+++ src/copy-demo.py
@@ -13,3 +13,16 @@ b[2].append(7)
 print("浅拷贝.............")
 print("a:", a)
 print("b:", b)
+
+c = [30, 40, [5, 6]]
+d = copy.deepcopy(c)
+
+print("c:", c)
+print("d:", d)
+
+print("深拷贝.............")
+
+d.append(40)
+d[2].append(7)
+print("c:", c)
+print("d:", d)
